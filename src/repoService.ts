9640d281a6cd4d2a737ce16239f5f303a5d9c40c
Christian <csamaniego@stackbuilders.com>
2022-04-04 10:42:35 -0500

    migrate to typescript

--- src/repoService.ts
+++ src/repoService.ts
@@ -1,18 +1,25 @@
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
+
+interface Repository {
+  id: number;
+  name: string;
+  stargazers_count: number;
+  updated_at: Date;
+}
 
 //get all repos from url
-const getRepos = async (url) => {
+const getRepos = async (url: string) => {
   let response = await fetch(url);
 
   if (response.status == 200) {
     let repos = await response.json();
     return repos;
   }
-  throw new Error(response.status);
+  throw new Error(response.statusText);
 }
 
 //get 5 star repositories from list
-const getFiveStarRepos = (repos) => {
+const getFiveStarRepos = (repos: Array<Repository>) => {
   if (!Array.isArray(repos)) throw new Error('Invalid repos object');
 
   const filteredRepos = repos.filter(repo => repo.stargazers_count >= 5)
@@ -20,19 +27,18 @@ const getFiveStarRepos = (repos) => {
 }
 
 //get 5 most recently updated repositories from list
-const getLastUpdatedRepos = (repos) => {
+const getLastUpdatedRepos = (repos: Array<Repository>) => {
   if (!Array.isArray(repos)) throw new Error('Invalid repos object');
-  sortRepos(repos)
-  .slice(0, 5)
+  return sortRepos(repos).slice(0, 5)
 }
 
 //sort repos list
-const sortRepos = (repos) => {
-  return repos.sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at));
+const sortRepos = (repos: Array<Repository>) => {
+  return repos.sort((a, b) => (new Date(b.updated_at)).valueOf() - (new Date(a.updated_at)).valueOf());
 }
 
 //get total stars from a list of repositories
-const getTotalStars = (repos) => {
+const getTotalStars = (repos: Array<Repository>) => {
   if (!Array.isArray(repos)) throw new Error('Invalid repos object');
 
   return repos.reduce((acc, el) => {
@@ -41,8 +47,8 @@ const getTotalStars = (repos) => {
 }
 
 //for visibility only
-const mapFields = (repos) => {
-  repos.map(repo => ({
+const mapFields = (repos: Array<Repository>) => {
+  return repos.map(repo => ({
     id: repo.id,
     name: repo.name,
     stargazers_count: repo.stargazers_count,
@@ -50,16 +56,16 @@ const mapFields = (repos) => {
   }));
 }
 
-const main = async (url) => {
+const main = async (url: string) => {
   try {
     const repos = await getRepos(url);
-    const fiveStarRepos = getFiveStarRepos(repos);
+    const fiveStarRepos = getFiveStarRepos(repos as Array<Repository>);
     console.log('-----REPOSITORIES WITH MORE THAN 5 STARS-----')
     console.log(mapFields(fiveStarRepos));
-    const lastUpdated = getLastUpdatedRepos(repos)
+    const lastUpdated = getLastUpdatedRepos(repos as Array<Repository>)
     console.log('-----LAST FIVE UPDATED REPOSITORIES----')
     console.log(mapFields(lastUpdated));
-    const totalStars = getTotalStars(repos);
+    const totalStars = getTotalStars(repos as Array<Repository>);
     console.log('-----SUM OF ALL REPOSITORY STARS----')
     console.log(totalStars);
   } catch (err) {
@@ -69,10 +75,3 @@ const main = async (url) => {
 
 const sbUrl = 'https://api.github.com/orgs/stackbuilders/repos';
 main(sbUrl);
-
-module.exports = {
-  getRepos,
-  getFiveStarRepos,
-  getLastUpdatedRepos,
-  getTotalStars
-}
